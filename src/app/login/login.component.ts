bdad246a2ce7744b114835dae2b20cb8dc5c304a
Riddhesh Naik <riddheshnaik786@gmail.com>
2022-02-06 23:22:48 +0530

    frontend commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,21 +18,7 @@ import { Admin } from '../admin';
 })
 export class LoginComponent implements OnInit {
 
-  username: String;
-  password: String;
-
-  email: String;
-  pass: String;
-
-  msg = ''; 
-
-  public sellers: Seller[];
-
-  public buyers: Buyer[];
-
-  public admin: Admin[];
-  
-  
+ 
 
   constructor( private router: Router, private loginService: RegistrationService ) { }
 
@@ -40,84 +26,5 @@ export class LoginComponent implements OnInit {
   
   }
 
-  
-  
-  // loginUser(loginForm: NgForm)
-  // { 
 
-  //   this.loginService.loginAdmin(loginForm.value)
-  //   .subscribe(
-  //     res => {
-  //       console.log(res);
-      
-  //     this.router.navigate(['admin'])
-  //   },
-
-  //     err => {
-  //       console.log(err) 
-  //       this.msg="Bad Credentials";
-  //     }
-  //   )
-
-
-
-    // for(const buyer of this.buyers)
-    // {
-    //   if ((buyer.email !== null)) {
-    //     console.log(this.email);
-    //   }
-    
-    // }
-    
-    
-    // this.email = (<HTMLInputElement>document.getElementById("email")).value;
-
-    // this.pass = (<HTMLInputElement>document.getElementById("pass")).value;
- 
-    // // if(this.email === this.buyers.email && this.pass === this.buyers.password)
-
-    // console.log(this.email);
-    // console.log(this.pass);
-
-
-
-    // this.loginService.loginBuyerUser(loginForm.value)
-    // .subscribe(
-    //   res => {
-    //     console.log(res);
-      
-    //   this.router.navigate(['bidder'])
-    // },
-
-    //   err => {
-    //     console.log(err) 
-    //     this.msg="Bad Credentials";
-    //   }
-    // )
-    
-  
-  // }
-
-
-  
-
-  // loginBidderUser(loginForm: NgForm)
-  // {
-  //   this.loginService.loginSellerUser(loginForm.value)
-  //   .subscribe(
-  //     res => {
-  //       console.log(res);
-        
-  //     this.router.navigate(['seller'])
-  //   },
-
-  //     err => {
-  //       console.log(err) 
-  //       this.msg="Bad Credentials";
-  //     }
-  //   )
-
-  // }
-
- 
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -13,32 +13,10 @@ import { Router } from '@angular/router';
 })
 export class RegistrationComponent implements OnInit {
 
-  public buyer: Buyer[]; 
-
-  
- 
   
   constructor(private auth: RegistrationService , private router: Router) { }
 
   ngOnInit() {
   }
-
-  // const jwt = require('angular-jwt')
-
-
-  // public registerUser(addForm: NgForm)
-  // {
-    
-  //   this.auth.registerBuyerUser(addForm.value)
-  //   .subscribe(
-  //     res => {
-  //       console.log(res);
-  //       // localStorage.setItem('token', res.token)
-  //       this.router.navigate(['']);
-
-  //     },
-  //     err => console.log(err)
-  //   )
-  // }
 
 }
